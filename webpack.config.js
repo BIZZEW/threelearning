4f52978acdf6c4315151e3a2e27c2a9342d10075
obwan <azurazazel@gmail.com>
2020-04-21 15:02:40 +0800

    webpack 自动发现 entry 的配置和引用方式

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
-var webpack = require("webpack")
-var path = require('path')
+var webpack = require("webpack");
+var path = require('path');
+var path = require('path');
+var glob = require('glob');
+
+function entries(globPath) {
+    var files = glob.sync(globPath);
+    var entries = {}, entry, dirname, basename;
+
+    for (var i = 0; i < files.length; i++) {
+        entry = files[i];
+        dirname = path.dirname(entry);
+        basename = path.basename(entry, '.js');
+        entries[path.join(dirname, basename)] = './' + entry;
+    }
+    return entries;
+}
+
 module.exports = {
-    entry: {
-        firstThree: './src/firstThree.js',
-        secondThree: './src/secondThree.js',
-        thirdThree: './src/thirdThree.js',
-        BufferGeometry: './src/BufferGeometry.js',
-    },
+    // entry: {
+    //     firstThree: './src/firstThree.js',
+    //     secondThree: './src/secondThree.js',
+    //     thirdThree: './src/thirdThree.js',
+    //     BufferGeometry: './src/BufferGeometry.js',
+    // },
+    entry: entries('./src/*.js'),
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
