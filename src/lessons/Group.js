661de8f951dbe3f03c87fc1a2dbceda49c2df981
obwan <azurazazel@gmail.com>
2020-05-12 16:47:32 +0800

    组对象Group、层级模型

---
+++ src/lessons/Group.js
@@ -0,0 +1,100 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+var scene = new THREE.Scene();
+
+//创建两个网格模型mesh1、mesh2
+var geometry = new THREE.BoxGeometry(20, 20, 20);
+var material = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+var group = new THREE.Group();
+var mesh1 = new THREE.Mesh(geometry, material);
+var mesh2 = new THREE.Mesh(geometry, material);
+mesh2.translateX(25);
+//把mesh1型插入到组group中，mesh1作为group的子对象
+group.add(mesh1, mesh2);
+//把mesh2型插入到组group中，mesh2作为group的子对象
+group.add(mesh2);
+//把group插入到场景中作为场景子对象
+scene.add(group);
+
+//沿着Y轴平移mesh1和mesh2的父对象，mesh1和mesh2跟着平移
+group.translateY(100);
+
+//父对象缩放，子对象跟着缩放
+group.scale.set(4, 4, 4);
+
+//父对象旋转，子对象跟着旋转
+group.rotateY(Math.PI / 4);
+
+console.log('查看group的子对象', group.children);
+
+console.log('查看Scene的子对象', scene.children);
+
+group.remove(mesh1);
+
+
+
+/**
+ * 光源设置
+ */
+
+//点光源
+var point = new THREE.PointLight(0xffffff);
+point.position.set(200, 200, 200); //点光源位置
+// 通过add方法插入场景中，不插入的话，渲染的时候不会获取光源的信息进行光照计算
+scene.add(point); //点光源添加到场景中
+
+// 点光源2  位置和point关于原点对称
+// var point2 = new THREE.PointLight(0xffffff);
+// point2.position.set(-400, -200, -300); //点光源位置
+// scene.add(point2); //点光源添加到场景中
+
+//环境光    环境光颜色与网格模型的颜色进行RGB进行乘法运算
+var ambient = new THREE.AmbientLight(0x444444);
+// var ambient = new THREE.AmbientLight(0xffffff);
+scene.add(ambient);
+
+
+// console.log(scene)
+// console.log(scene.children)
+
+/**
+ * 相机设置
+ */
+var width = window.innerWidth; //窗口宽度
+var height = window.innerHeight; //窗口高度
+var k = width / height; //窗口宽高比
+var s = 300; //三维场景显示范围控制系数，系数越大，显示的范围越大
+
+//创建相机对象
+var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+camera.position.set(300, 300, 300); //设置相机位置
+camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+
+/**
+ * 创建渲染器对象
+ */
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);//设置渲染区域尺寸
+renderer.setClearColor(0xb9d3ff, 1); //设置背景颜色
+document.body.appendChild(renderer.domElement); //body元素中插入canvas对象
+
+// function render() {
+//     renderer.render(scene, camera);//执行渲染操作
+// }
+// render();
+
+// var controls = new OrbitControls(camera, renderer.domElement);
+// controls.addEventListener('change', render);//监听鼠标、键盘事件
+
+function render() {
+    renderer.render(scene, camera);//执行渲染操作
+    requestAnimationFrame(render);//请求再次执行渲染函数render
+}
+render();
+
+var controls = new OrbitControls(camera, renderer.domElement);
+
+// 辅助坐标系  参数250表示坐标系大小，可以根据场景大小去设置
+var axisHelper = new THREE.AxesHelper(250);
+scene.add(axisHelper);
