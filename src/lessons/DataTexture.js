57c671cf91cb6ed0cbc388c9f5b535d9dde6b5f6
obwan <azurazazel@gmail.com>
2020-05-25 09:50:58 +0800

    正投影相机OrthographicCamera和透视投影相机PerspectiveCamera

--- src/lessons/DataTexture.js
+++ src/lessons/DataTexture.js
@@ -105,14 +105,24 @@ scene.add(ambient);
 /**
  * 相机设置
  */
+// var width = window.innerWidth; //窗口宽度
+// var height = window.innerHeight; //窗口高度
+// var k = width / height; //窗口宽高比
+// var s = 300; //三维场景显示范围控制系数，系数越大，显示的范围越大
+
+// //创建相机对象
+// var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+// camera.position.set(300, 300, 300); //设置相机位置
+// camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+
+/**
+ * 透视投影相机设置
+ */
 var width = window.innerWidth; //窗口宽度
 var height = window.innerHeight; //窗口高度
-var k = width / height; //窗口宽高比
-var s = 300; //三维场景显示范围控制系数，系数越大，显示的范围越大
-
-//创建相机对象
-var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
-camera.position.set(300, 300, 300); //设置相机位置
+/**透视投影相机对象*/
+var camera = new THREE.PerspectiveCamera(60, width / height, 1, 2000);
+camera.position.set(20, 20, 20); //设置相机位置
 camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
 
 /**
